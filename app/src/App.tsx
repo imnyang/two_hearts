ee8760a6ff04697f111ad15af5a6883f4c5012b4
imnyang <me@imnya.ng>
2025-03-23 22:50:28 +0900

    구글아 다해줬다.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SearchBar from "./components/searchbar";
 import "./index.css";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./components/ui/tooltip";
@@ -6,25 +6,69 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Settings } from "lucide-react";
+import { Label } from "./components/ui/label";
+import { Checkbox } from "./components/ui/checkbox";
+import { Input } from "./components/ui/input";
+import Clock from "./components/Clock";
 
 export function App() {
   const [copyright, setCopyRight] = useState([]);
-  const [time, setTime] = useState(new Date());
-  const userLanguage = navigator.language.split('-')[0];
+  const [disableAutocomplete, setDisableAutoComplete] = useState(() => {
+    const storedAutoComplete = localStorage.getItem("disableautocomplete");
+    return storedAutoComplete === "true";
+  });
+  const [is24Hour, setIs24Hour] = useState(() => {
+    const storedFormat = localStorage.getItem("is24Hour");
+    return storedFormat === "true";
+  });
+  const [BingChecked, setBingChecked] = useState(() => {
+    const storedBingChecked = localStorage.getItem("customWallpaper");
+    return storedBingChecked === "true";
+  });
+  const [wallpaper, setWallpaper] = useState<string | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    if (BingChecked === true && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [BingChecked]);
+
+  const userLanguage = navigator.language.split('-')[0];
+
+  const handleCheckboxChange = (checked: boolean) => {
+    setBingChecked(checked);
+    if (!checked) {
+      localStorage.removeItem("customWallpaper");
+    } else {
+      localStorage.setItem("customWallpaper", "true");
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    localStorage.setItem("customWallpaperURL", value);
+    setWallpaper(value);
+  }
 
   const fetchWallpaper = async () => {
+    const customWallpaper = localStorage.getItem("customWallpaperURL");
+    if (customWallpaper) {
+      setWallpaper(customWallpaper);
+      return customWallpaper;
+    }
+    const BingWallpaper = localStorage.getItem("customWallpaper");
+    if (BingWallpaper) {
+      setBingChecked(true);
+      return BingWallpaper;
+    }
+
     const cachedWallpaper = localStorage.getItem("wallpaper");
     const cachedCopyRight = localStorage.getItem("copyright");
     const cachedCopyRightLink = localStorage.getItem("copyright_link");
@@ -60,22 +104,19 @@ export function App() {
     <div className="flex flex-col items-center justify-center h-screen">
       <img 
       alt="bing-wallpaper" 
-      className="object-cover w-full h-full absolute blur-xs transition-opacity duration-1000 opacity-0" 
+      className={`object-cover w-full h-full absolute blur-xs transition-opacity duration-1000 ${BingChecked ? 'hidden' : 'opacity-0'}`} 
       onLoad={(e) => {
       const img = e.currentTarget as HTMLImageElement;
       if (img.naturalWidth < 1920) {
-        img.src = "http://bingw.jasonzeng.dev/?index=random";
+      img.src = "http://bingw.jasonzeng.dev/?index=random";
       } else {
-        img.style.opacity = '1';
+      img.style.opacity = '1';
       }
       }}
       />
       <div className="flex flex-col items-center justify-center h-full">
-        <div className="relative mb-4 flex flex-row items-end">
-          <h1 className="text-4xl font-extrabold">{time.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: false })}</h1>
-            <p className="text-2xl pb-0.5">:{time.toLocaleTimeString('ko-KR', { second: '2-digit', hour12: false }).padStart(2, '0')}</p>
-          
-        </div>
+        <Clock />
+
         <SearchBar />
       </div>
       <div className="absolute bottom-4 right-4 flex flex-col items-end gap-1">
@@ -85,9 +126,82 @@ export function App() {
             <DialogHeader>
               <DialogTitle>Settings</DialogTitle>
               <DialogDescription>
-                
+                <div className="flex flex-col mt-4 gap-4">
+                  <div className="flex items-start gap-2">
+                    <Checkbox
+                      checked={BingChecked}
+                      onCheckedChange={handleCheckboxChange}
+
+                    />
+                    <div className="grow">
+                      <div className="grid gap-2">
+                        <Label className="text-foreground">Don't use Bing Backgrond</Label>
+                        <p className="text-muted-foreground text-xs">
+                          To customize the background, insert a direct link to the image you want.<br/>
+                          If you leave this field blank, the default background color will be used.
+                        </p>
+                      </div>
+                      {/* Expandable field */}
+                      <div
+                        role="region"
+                        className="grid transition-all ease-in-out data-[state=collapsed]:grid-rows-[0fr] data-[state=collapsed]:opacity-0 data-[state=expanded]:grid-rows-[1fr] data-[state=expanded]:opacity-100"
+                        data-state={BingChecked ? "expanded" : "collapsed"}
+                      >
+                        <div className="pointer-events-none -m-2 overflow-hidden p-2">
+                          <div className="pointer-events-auto mt-3">
+                            <Input
+                              ref={inputRef}
+                              type="text"
+                              id="checkbox-11-additional-info"
+                              placeholder="Image Direct Link"
+                              aria-label="Additional Information"
+                              onChange={handleInputChange}
+                              defaultValue={wallpaper || ""}
+                              disabled={!BingChecked}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2">
+                  <Checkbox
+                    checked={is24Hour}
+                    onCheckedChange={(is24HourEvent) => {
+                    if (typeof is24HourEvent === "boolean") {
+                      setIs24Hour(is24HourEvent);
+                    }
+                    if (is24HourEvent) {
+                    localStorage.setItem("is24Hour", "true");
+                    } else {
+                    localStorage.removeItem("is24Hour");
+                    }
+                    }}
+                  />
+                  <Label className="text-foreground">Use 24 hour time format</Label>
+                  </div>
+                  <div className="flex items-center gap-2">
+                  <Checkbox
+                    checked={disableAutocomplete}
+                    onCheckedChange={(disableAutocompleteEvent) => {
+                    if (typeof disableAutocompleteEvent === "boolean") {
+                      setDisableAutoComplete(disableAutocompleteEvent);
+                    }
+                    if (disableAutocompleteEvent) {
+                    localStorage.setItem("disableautocomplete", "true");
+                    } else {
+                    localStorage.removeItem("disableautocomplete");
+                    }
+                    }}
+                  />
+                  <Label className="text-foreground">Disable Auto Search Suggestion</Label>
+                  </div>
+                </div>
               </DialogDescription>
             </DialogHeader>
+            <DialogFooter className="flex flex-row gap-2">
+              <Label className="text-muted-foreground">All settings will be applied when you reopen this tab.</Label>
+            </DialogFooter>
           </DialogContent>
         </Dialog>
 
